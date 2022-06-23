c08ff82da4b59115f815acb89b76a95ca666104c
jimmy-oss <jimmynjonge51@gmail.com>
2022-06-23 15:24:13 +0300

    riverside project

---
+++ hospitals/models.py
@@ -0,0 +1,56 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+import uuid
+from datetime import datetime
+
+# Create your models here.
+User = get_user_model() 
+
+class Doctor(models.Model):
+    name = models.CharField(max_length=50)
+    mobile = models.IntegerField()
+    special = models.CharField(max_length=50)
+
+    def __str__(self):
+       return self.name
+
+class Patient(models.Model):
+    name = models.CharField(max_length=50)
+    gender = models.CharField(max_length=10)
+    mobile = models.IntegerField(null=True)
+    address = models.CharField(max_length=50)
+
+    def __str__(self):
+       return self.name
+
+class Appointment(models.Model):
+    doctor = models.ForeignKey(Doctor,on_delete=models.CASCADE)
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    date1 = models.DateField()
+    time1 = models.TimeField()
+
+    def __str__(self):
+       return self.doctorname+"--"+self.patient.name
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100, null=True)
+    contact = models.CharField(max_length=15, null=True)
+    email = models.CharField(max_length=50, null=True)
+    subject = models.CharField(max_length=100, null=True)
+    message = models.CharField(max_length=300, null=True)
+    msgdate = models.DateField(null=True)
+    isread = models.CharField(max_length=10,null=True)
+
+    def __str__(self):
+        return self.id
+    
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE,related_name='profile')
+    id_user = models.IntegerField()
+    bio = models.TextField(blank=True)
+    email_address = models.EmailField(max_length=150,blank=True)
+    profileimg = models.ImageField(upload_to='profile_images', default='blank-profile-picture.png')
+    location = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.user.username
---
+++ hospitals/views.py
@@ -0,0 +1,288 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,logout,login
+from django.contrib import messages
+from django.contrib.auth.models import User,auth
+from django.contrib.auth.decorators import login_required
+from .models import *
+from datetime import date
+
+# Create your views here.
+@login_required(login_url='signin')
+def About(request):
+    return render(request,'about.html')
+
+@login_required(login_url='signin')
+def home(request):
+    return render(request,'home.html')
+
+@login_required(login_url='signin')
+def doctor(request):
+    return render(request ,'doctor.html')
+
+@login_required(login_url='signin')
+def department(request):
+    return render(request ,'department.html')
+
+def signup(request):
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email Taken')
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username Taken')
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+               
+
+                #log user in and redirect to settings page
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
+
+                #create a Profile object for the new user
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
+                new_profile.save()
+                return redirect('home')
+        else:
+            messages.info(request, 'Password Not Matching')
+            return redirect('signup')
+        
+    else:
+        return render(request, 'signup.html')
+    
+def signin(request):
+    
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Credentials Invalid')
+            return redirect('signin')
+
+    else:
+        return render(request, 'signin.html')   
+    
+@login_required(login_url='signin')
+def contact(request):
+    error = ""
+    if request.method == 'POST':
+        n = request.POST['name']
+        c = request.POST['contact']
+        e = request.POST['email']
+        s = request.POST['subject']
+        m = request.POST['message']
+        try:
+            Contact.objects.create(name=n, contact=c, email=e, subject=s, message=m, msgdate=date.today(), isread="no")
+            error = "no"
+        except:
+            error = "yes"
+    return render(request, 'contact.html', locals())
+
+@login_required(login_url='signin')
+def adminlogin(request):
+    error = ""
+    if request.method == 'POST':
+        u = request.POST['uname']
+        p = request.POST['pwd']
+        user = authenticate(username=u, password=p)
+        try:
+            if user.is_staff:
+                login(request, user)
+                error = "no"
+            else:
+                error = "yes"
+        except:
+            error = "yes"
+    return render(request,'login.html', locals())
+
+def admin_home(request):
+    if not request.user.is_staff:
+        return redirect('login_admin')
+    dc = Doctor.objects.all().count()
+    pc = Patient.objects.all().count()
+    ac = Appointment.objects.all().count()
+
+    d = {'dc': dc, 'pc': pc, 'ac': ac}
+    return render(request,'admin_home.html', d)
+
+def Logout(request):
+    logout(request)
+    return redirect('home')
+
+def add_doctor(request):
+    error=""
+    if not request.user.is_staff:
+        return redirect('login')
+    if request.method=='POST':
+        n = request.POST['name']
+        m = request.POST['mobile']
+        sp = request.POST['special']
+        try:
+            Doctor.objects.create(name=n,mobile=m,special=sp)
+            error="no"
+        except:
+            error="yes"
+    return render(request,'add_doctor.html', locals())
+
+def view_doctor(request):
+    if not request.user.is_staff:
+        return redirect('login')
+    doc = Doctor.objects.all()
+    d = {'doc':doc}
+    return render(request,'view_doctor.html', d)
+
+def Delete_Doctor(request,pid):
+    if not request.user.is_staff:
+        return redirect('login')
+    doctor = Doctor.objects.get(id=pid)
+    doctor.delete()
+    return redirect('view_doctor.html')
+
+def edit_doctor(request,pid):
+    error = ""
+    if not request.user.is_authenticated:
+        return redirect('login')
+    user = request.user
+    doctor = Doctor.objects.get(id=pid)
+    if request.method == "POST":
+        n1 = request.POST['name']
+        m1 = request.POST['mobile']
+        s1 = request.POST['special']
+
+        doctor.name = n1
+        doctor.mobile = m1
+        doctor.special = s1
+
+        try:
+            doctor.save()
+            error = "no"
+        except:
+            error = "yes"
+    return render(request, 'edit_doctor.html', locals())
+
+def add_patient(request):
+    error = ""
+    if not request.user.is_staff:
+        return redirect('login')
+    if request.method == 'POST':
+        n = request.POST['name']
+        g = request.POST['gender']
+        m = request.POST['mobile']
+        a = request.POST['address']
+        try:
+            Patient.objects.create(name=n, gender=g, mobile=m, address=a)
+            error = "no"
+        except:
+            error = "yes"
+    return render(request,'add_patient.html', locals())
+
+def view_patient(request):
+    if not request.user.is_staff:
+        return redirect('login')
+    pat = Patient.objects.all()
+    d = {'pat':pat}
+    return render(request,'view_patient.html', d)
+
+def Delete_Patient(request,pid):
+    if not request.user.is_staff:
+        return redirect('login')
+    patient = Patient.objects.get(id=pid)
+    patient.delete()
+    return redirect('view_patient.html')
+
+def edit_patient(request,pid):
+    error = ""
+    if not request.user.is_authenticated:
+        return redirect('login')
+    user = request.user
+    patient = Patient.objects.get(id=pid)
+    if request.method == "POST":
+        n1 = request.POST['name']
+        m1 = request.POST['mobile']
+        g1 = request.POST['gender']
+        a1 = request.POST['address']
+
+        patient.name = n1
+        patient.mobile = m1
+        patient.gender = g1
+        patient.address = a1
+        try:
+            patient.save()
+            error = "no"
+        except:
+            error = "yes"
+    return render(request, 'edit_patient.html', locals())
+
+
+
+def add_appointment(request):
+    error=""
+    if not request.user.is_staff:
+        return redirect('login')
+    doctor1 = Doctor.objects.all()
+    patient1 = Patient.objects.all()
+    if request.method=='POST':
+        d = request.POST['doctor']
+        p = request.POST['patient']
+        d1 = request.POST['date']
+        t = request.POST['time']
+        doctor = Doctor.objects.filter(name=d).first()
+        patient = Patient.objects.filter(name=p).first()
+        try:
+            Appointment.objects.create(doctor=doctor, patient=patient, date1=d1, time1=t)
+            error="no"
+        except:
+            error="yes"
+    d = {'doctor':doctor1,'patient':patient1,'error':error}
+    return render(request,'add_appointment.html', d)
+
+def view_appointment(request):
+    if not request.user.is_staff:
+        return redirect('login')
+    appointment = Appointment.objects.all()
+    d = {'appointment':appointment}
+    return render(request,'view_appointment.html', d)
+
+def Delete_Appointment(request,pid):
+    if not request.user.is_staff:
+        return redirect('login')
+    appointment1 = Appointment.objects.get(id=pid)
+    appointment1.delete()
+    return redirect('view_appointment.html')
+
+def unread_queries(request):
+    if not request.user.is_authenticated:
+        return redirect('login')
+    contact = Contact.objects.filter(isread="no")
+    return render(request,'unread_queries.html', locals())
+
+def read_queries(request):
+    if not request.user.is_authenticated:
+        return redirect('login')
+    contact = Contact.objects.filter(isread="yes")
+    return render(request,'read_queries.html', locals())
+
+def view_queries(request,pid):
+    if not request.user.is_authenticated:
+        return redirect('login')
+    contact = Contact.objects.get(id=pid)
+    contact.isread = "yes"
+    contact.save()
+    return render(request,'view_queries.html', locals())
+
