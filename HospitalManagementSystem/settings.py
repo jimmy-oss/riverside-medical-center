bec3f728ad968ff623cb89b492cdcb3c7b90867b
Wamaitha <wamaithaweru19@gmail.com>
2022-06-23 11:25:38 +0300

    initial commit

--- HospitalManagementSystem/settings.py
+++ HospitalManagementSystem/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'HospitalManagementSystem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hospital',
+        'USER':'wamaitha',
+        'PASSWORD': 'apple2020',
     }
 }
 
